8e8670c5107eb139fa6791ea833b22dfa6a2843a
Leevi Lautanen <leevi.lautanen02@gmail.com>
2023-11-03 18:52:52 +0200

    all tasks done

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ let container = document.createElement("div")
 container.classList.add("container")
 document.body.appendChild(container)
 
-const list = [["Staffordshire Bullterrier", "bullterrier"], ["Beagle", "beagle"], ["Finnish lapphund", "finnish"], ["Boxer", "boxer"], ["Corgi", "corgi"]]
+const list = [
+    ["Staffordshire Bullterrier", "bullterrier", "Staffordshire_Bull_Terrier"], 
+    ["Beagle", "beagle", "Beagle"], 
+    ["Finnish lapphund", "finnish", "Finnish_Lapphund"], 
+    ["Boxer", "boxer", "Boxer_(dog)"], 
+    ["Corgi", "corgi", "Corgi"]]
 
 for (dog of list) {
-    addItem(dog[0], dog[1])
+    addItem(dog[0], dog[1], dog[2])
 }
 
-function addItem(headerText, imgName) {
+function addItem(headerText, imgName, wikiName) {
     let item = document.createElement("div")
     item.classList.add("wiki-item")
     container.appendChild(item)
@@ -22,32 +27,39 @@ function addItem(headerText, imgName) {
     content.classList.add("wiki-content")
     item.appendChild(content)
 
-    let text = document.createElement("p")
-    text.classList.add("wiki-text")
-    text.textContent = "Text goes here. ".repeat(40)
-    content.appendChild(text)
-
     let imgContainer = document.createElement("div")
     imgContainer.classList.add("img-container")
     content.appendChild(imgContainer)
 
     let img = document.createElement("img")
     img.classList.add("wiki-img")
-    getImgUrl(img, imgName)
+    getImg(img, imgName)
     imgContainer.appendChild(img)
+
+    let text = document.createElement("p")
+    text.classList.add("wiki-text")
+    getSummary(text, wikiName)
+    text.textContent = "Text goes here. ".repeat(40)
+    content.appendChild(text)
+
+
     
 
 
 
 }
 
-async function getImgUrl(img, dog) {
+async function getImg(img, dog) {
     const response = await fetch("https://dog.ceo/api/breed/" + dog + "/images/random")
     const data = await response.json()
     img.src = data.message
 }
 
-
+async function getSummary(text, dog) {
+    const response = await fetch("https://en.wikipedia.org/api/rest_v1/page/summary/" + dog)
+    const data = await response.json()
+    text.textContent = data.extract
+}
 
 
 
